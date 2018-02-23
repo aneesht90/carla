0de1a1d55ca29cfbb0ccb5b37b86ab88ab44dcf6
Aneesh Thattanthody <aneesht90@gmail.com>
2018-02-23 09:14:43 +0100

    model code clearup

--- PythonClient/Utils/nnet.py
+++ PythonClient/Utils/nnet.py
@@ -13,46 +13,8 @@ def easy_drive():
     """
     model = Sequential()
     model.add(Dense(10, activation="relu",input_dim=2))
-    #model.add(Dropout(0.8))
-    #model.add(Dense(100, activation="relu"))
-    #model.add(Dense(16, activation="relu"))
-    #model.add(Dense(10, activation="relu"))
-    model.add(Dropout(0.2))
-    model.add(Dense(2))
-    model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
-    return model
-
-def create_model_neurons(neurons=10):
-	# create model
-    model = Sequential()
-    model.add(Dense(neurons, activation="relu",input_dim=2))
-    model.add(Dropout(0.2))
-    model.add(Dense(2))
-	# Compile model
-    model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
-    #model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
-
-
-
-def test_model():
-    model = Sequential()
-    model.add(Flatten(input_shape=(67, 320)))
-    model.add(Dense(1))
-    model.compile(loss="mse", optimizer="adam")
-    return model
-
-def drive_with_camera():
-    model = Sequential()
-    model.add(Conv2D(24, (5, 5), strides=(2, 2), activation="relu", input_shape=(67, 320, 3)))
-    model.add(Conv2D(36, (5, 5), strides=(2, 2), activation="relu"))
-    model.add(Conv2D(48, (5, 5), strides=(2, 2), activation="relu"))
-    model.add(Conv2D(64, (3, 3), strides=(1, 1), activation="relu"))
-    model.add(Conv2D(64, (3, 3), strides=(1, 1), activation="relu"))
-    model.add(Flatten())
-    model.add(Dense(100, activation="relu"))
-    model.add(Dense(50, activation="relu"))
     model.add(Dense(10, activation="relu"))
-    model.add(Dense(1))
+    #model.add(Dropout(0.9))
+    model.add(Dense(2))
     model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
     return model
